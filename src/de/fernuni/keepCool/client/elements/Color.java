49a401d60d4ca8bdeac8a7cbac15603bb940c97a
MissJule <moni.schrenk@gmail.com>
2014-05-27 21:48:55 +0200

    added 2 colors, changed logic of dice

--- src/de/fernuni/keepCool/client/elements/Color.java
+++ src/de/fernuni/keepCool/client/elements/Color.java
@@ -3,11 +3,13 @@ package de.fernuni.keepCool.client.elements;
 /**
  * @author fernuni
  * 
- *         Enum repräsentiert die Spielfarben, sollte für 6 Spieler erweitert
- *         werden ;-) ... mit Farben die auch ich gucken kann - Jörg
+ *         Enum repraesentiert die Spielfarben fuer 6 Spieler  ;-) ... 
+ *         mit Farben die auch Joerg gucken kann      
+ * 
  */
 public enum Color {
-	BLACK("0x000000"), BLUE("0x0000FF"), RED("0xFF0000"), YELLOW("0xFFFF00");
+	BLACK("0x000000"), BLUE("0x0000FF"), RED("0xFF0000"), YELLOW("0xFFFF00"), 
+	TURQUOISE("0x39C3DF"), GREY("0xACACAC");
 
 	String value;
 
